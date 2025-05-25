da56e7b3440cd45b38ec747630b28568f801416a
Amey Kulkarni <ameykulkarni@live.in>
2025-05-25 21:05:22 +0530

    sample api endpoint and response tested

--- url_shortener.go
+++ url_shortener.go
@@ -1,1 +1,28 @@
-package url_shortener_assignment_infracloud
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Hello world, the web server
+
+	shortenUrlHandler := func(w http.ResponseWriter, req *http.Request) {
+		_, _ = io.WriteString(w, "https://exm.pl/abc123")
+	}
+
+	longUrlHandler := func(w http.ResponseWriter, req *http.Request) {
+		_, _ = io.WriteString(w, "https://google.com/search?q=fluffy+cat")
+	}
+
+	metricHandler := func(w http.ResponseWriter, req *http.Request) {
+		_, _ = io.WriteString(w, "{\"youtube\" : 1}")
+	}
+	http.HandleFunc("POST /shorten", shortenUrlHandler)
+	http.HandleFunc("GET /metric", metricHandler)
+	http.HandleFunc("GET /{shortened_url}", longUrlHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
